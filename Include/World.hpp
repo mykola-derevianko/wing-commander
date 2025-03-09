9e89fc1b1dacfcd5abf5658f98bf59776c3faae0
MykolaDerevianko <derevianko.work@gmail.com>
2025-03-09 20:53:43 +0100

    World with tile generation is half implemented

---
+++ Include/World.hpp
@@ -0,0 +1,16 @@
+//
+// Created by derev on 3/9/2025.
+//
+
+#ifndef SFML_GAME_WORLD_HPP
+#define SFML_GAME_WORLD_HPP
+
+
+
+class World {
+
+};
+
+
+
+#endif //SFML_GAME_WORLD_HPP
---
+++ src/World.cpp
@@ -0,0 +1,5 @@
+//
+// Created by derev on 3/9/2025.
+//
+
+#include "World.hpp"
